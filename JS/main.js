5c41e0967fee145fdb3c6bb46488b7e79ccb85cd
TanaySharma21 <93471396+TanaySharma21@users.noreply.github.com>
2021-12-10 20:13:00 +0530

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -8,54 +8,44 @@
         $(".navbar-collapse").collapse('hide');
     });
 
-    // REVIEWS CAROUSEL
-    $('.reviews-carouselb').owlCarousel({
-        center: true,
-        loop: true,
-        nav: true,
-        dots: false,
-        autoplay: true,
-        autoplaySpeed: 300,
-        smartSpeed: 500,
-        responsive:{
-          0:{
-            items:1,
-          },
-          768:{
-            items:2,
-            margin: 100,
-          },
-          1280:{
-            items:2,
-            margin: 100,
-          }
-        }
-    });
-
     // Banner Carousel
     var myCarousel = document.querySelector('#myCarousel')
     var carousel = new bootstrap.Carousel(myCarousel, {
       interval: 1500,
     })
 
-    // REVIEWS NAVIGATION
-    function ReviewsNavResize(){
-      $(".navbar").scrollspy({ offset: -94 });
-
-      var ReviewsOwlItem = $('.reviews-carousel .owl-item').width();
-
-      $('.reviews-carousel .owl-nav').css({'width' : (ReviewsOwlItem) + 'px'});
-    }
-
-    $(window).on("resize", ReviewsNavResize);
-    $(document).on("ready", ReviewsNavResize);  
-
     // HREF LINKS
+    // Binding an event handler to all anchors that contain
+    // a hash (#), but not necessarily JUST a hash - like href="#"
+    // which is typically used in JS...
     $('a[href*="#"]').click(function (event) {
+
+      // Two conditional checks
+      // First:
+      // location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      // What we're doing is replacing the first forward slash (/) in the pathname
+      // for the current location and comparing it to the link that's been clicked.
+      // So http://personalsite.com/test/link/src, which normally would have
+      // a pathname of /test/link/src would be test/link/src
+
+      // The or check (||) is to see if the link matches the current domain
+      // location.hostname == this.hostname
+
+      // Basically, we want an internal anchor for the page we're on.
       if (
         location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+
+        // Assigning the variable target, with the hash of the link that's been clicked
+        // which is conveniently enough, a jquery selector for IDs (i.e. #hash)
         var target = $(this.hash);
+
+        // We check the target length - basically, does the element exist?
+        // If length equals to 0, we query the DOM by name attribute. Otherwise, we just re-assign target to self.
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+
+        // The rest is self explanatory... (animation  using the target's offset)
+        // The return false prevents default behavior
         if (target.length) {
           event.preventDefault();
           $('html, body').animate({
@@ -63,6 +53,5 @@
           }, 1000);
         }
       }
-    });
-    
+    });    
   })
