df0ab30eb10dc6bfc0bb52e978eb33be4e9e31e2
TanaySharma21 <93471396+TanaySharma21@users.noreply.github.com>
2021-12-10 20:45:27 +0530

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -15,32 +15,11 @@
     })
 
     // HREF LINKS
-    // Binding an event handler to all anchors that contain
-    // a hash (#), but not necessarily JUST a hash - like href="#"
-    // which is typically used in JS...
     $('a[href*="#"]').click(function (event) {
-
-      // Two conditional checks
-      // First:
-      // location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      // What we're doing is replacing the first forward slash (/) in the pathname
-      // for the current location and comparing it to the link that's been clicked.
-      // The and check (&&) is to see if the link matches the current domain
-      // location.hostname == this.hostname
-
-      // Basically, we want an internal anchor for the page we're on.
       if (
         location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-
-        // Assigning the variable target, with the hash of the link that's been clicked
         var target = $(this.hash);
-
-        // We check the target length - basically, does the element exist?
-        // If length equals to 0, we query the DOM by name attribute. Otherwise, we just re-assign target to self.
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-
-        // The rest is self explanatory... (animation using the target's offset)
-        // The return false prevents default behavior
         if (target.length) {
           event.preventDefault();
           $('html, body').animate({
